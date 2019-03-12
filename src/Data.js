b97a615cef998edf2c2dc36043c15c725610085d
aivantg <aivant.developer@gmail.com>
2019-03-12 13:57:31 -0700

    added Data.js

---
+++ src/Data.js
@@ -0,0 +1,24 @@
+export const ProductData = { 
+    products: [ 
+        {
+            name: "Apple",
+            cost: 1.0,
+            stock: 100
+        },
+        {
+            name: "Banana",
+            cost: 0.5,
+            stock: 20
+        },
+        {
+            name: "Orange",
+            cost: 2.0,
+            stock: 5
+        },
+        {
+            name: "Durian",
+            cost: 5.0,
+            stock: 0
+        },
+    ]
+}
